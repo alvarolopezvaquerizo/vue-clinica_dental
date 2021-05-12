038cc3e4e399c0be00aa2523e43f0fa1616e8d01
Alvaro Lopez Vaquerizo <alvarolopezvaquerizo@hotmail.com>
2021-05-12 16:12:09 +0200

    bootstrap-vue installation, application icon change, restructuring the project, modifying routes

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-navbar class="navbar" type="dark" variant="dark">
+    <b-navbar-nav>
+      <b-nav-item><i class="far fa-user-circle"></i></b-nav-item>
+      <b-nav-item>Dr. Pablo Kehyaian</b-nav-item>
+      <b-nav-item><i class="fas fa-angle-down"></i></b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+
+
+
+<script>
+
+</script>
+
+
+
+<style scoped>
+.navbar {
+    justify-content: flex-end;
+}
+</style>
---
+++ src/components/Panel.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="panel">
+
+    </div>
+</template>
+
+
+
+<script>
+
+</script>
+
+
+
+<style scoped>
+.panel {
+    width: 100%;
+    min-height: 94vh;
+    border-right: 1px solid rgb(214, 214, 214);
+}
+</style>
